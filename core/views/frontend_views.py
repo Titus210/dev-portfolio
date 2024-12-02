403348aaf28a1b5e37bbb113fc6efaab8d870f01
Titus210 <tituskiplagat210@gmail.com>
2024-12-02 11:30:35 +0300

    - implemented frontend views to handle pages rendering -  deleted views.py since views(folder) is set as package __init__py

--- core/views/frontend_views.py
+++ core/views/frontend_views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, '../templates/home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, '../templates/about.html')
 
-from .models import Project
+from core.models import Project
 def projects(request):
     projects = Project.objects.all()
-    return render(request, 'projects.html', {'projects': projects})
+    return render(request, '../templates/projects.html', {'projects': projects})
 
-from .models import Contact
+from core.models import Contact
 from django.shortcuts import redirect
 
 def contact(request):
@@ -21,10 +21,10 @@ def contact(request):
         message = request.POST['message']
         Contact.objects.create(name=name, email=email, message=message)
         return redirect('home')  # Redirect after submission
-    return render(request, 'contact.html')
+    return render(request, '../templates/contact.html')
 
-from .models import BlogPost
+from core.models import BlogPost
 
 def blog(request):
     posts = BlogPost.objects.all()
-    return render(request, 'blog.html', {'posts': posts})
+    return render(request, '../templates/blog.html', {'posts': posts})
